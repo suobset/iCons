8b8591370fdc2ed2415e86054def2cb84f012d8a
Kushagra Srivastava <kushagra2410@gmail.com>
2021-04-28 03:57:39 -0400

    Implemented other pages

--- js/dropDown.js
+++ js/dropDown.js
@@ -1,7 +1,1 @@
-$('#dropdown').change(function(){
-    $('#textBoxContainer').empty();
-    var number = $(this).find('option:selected').attr('data-number');
-    for (var i = 0; i < number; i++){
-          $('#textBoxContainer').append('<input type="text"/>');
-    }
-});
+//Implement a nice data viewer here
--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-//Made for testing purposes only
-
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
